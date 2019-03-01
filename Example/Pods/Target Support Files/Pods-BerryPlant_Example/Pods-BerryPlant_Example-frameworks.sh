00ad81a89ab26e7f1fc6bf64579b24e7a786d333
坏人 <czqasn_6@163.com>
2019-03-01 14:21:48 +0800

    添加YYAsyncLayer

--- Example/Pods/Target Support Files/Pods-BerryPlant_Example/Pods-BerryPlant_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-BerryPlant_Example/Pods-BerryPlant_Example-frameworks.sh
@@ -154,9 +154,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/BerryPlant/BerryPlant.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/YYAsyncLayer/YYAsyncLayer.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/BerryPlant/BerryPlant.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/YYAsyncLayer/YYAsyncLayer.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
